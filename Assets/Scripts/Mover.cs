bc4ae5f969fd3beffe7f3760c42ff6ca4b8fc746
Tabare Sanchez <tabaregabriel@gmail.com>
2023-09-09 17:14:37 -0300

    Animaciones del jugador y sprite de los enemigos

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -14,21 +14,43 @@ public class Mover : MonoBehaviour
 
     // Variable para referenciar otro componente del objeto
     private Rigidbody2D miRigidbody2D;
+    
+    private Animator miAnimator;
+    private SpriteRenderer miSprite;
+    private CircleCollider2D miCollider2D;
+
+    private int saltarMask;
 
     // Codigo ejecutado cuando el objeto se activa en el nivel
     private void OnEnable()
     {
         miRigidbody2D = GetComponent<Rigidbody2D>();
+
+        miAnimator = GetComponent<Animator>();
+        miSprite = GetComponent<SpriteRenderer>();
+        miCollider2D = GetComponent<CircleCollider2D>();
+        saltarMask = LayerMask.GetMask("Pisos", "Plataformas");
     }
 
     // codigo ejecutado en cada frame del juego (Intervalo variable)
     private void Update()
     {
-        moverHorizontal = Input.GetAxis("Horizontal");
+        moverHorizontal = Input.GetAxisRaw("Horizontal");
         direccion = new Vector2(moverHorizontal, 0f);
-    }    
-   private void FixedUpdate()
+
+        int velocidadX = (int)miRigidbody2D.velocity.x;
+        miSprite.flipX = velocidadX > 0;
+        miAnimator.SetInteger("Velocidad", velocidadX);
+
+        miAnimator.SetBool("EnAire", !EnContactoConPlataforma());
+    }
+    private void FixedUpdate()
     {
         miRigidbody2D.AddForce(direccion * velocidad);
+    }
+
+    private bool EnContactoConPlataforma()
+    {
+        return miRigidbody2D.IsTouchingLayers(saltarMask);
     }
 }
