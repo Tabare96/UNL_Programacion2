71082cbe5df6ae943eff9f5c6b14c74a713a8fd8
Tabare Sanchez <tabaregabriel@gmail.com>
2023-10-30 09:50:07 -0300

    Cuando toca la lava, se reinicia el nivel

--- Assets/Scripts/Jugador/Jugador.cs
+++ Assets/Scripts/Jugador/Jugador.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class Jugador : MonoBehaviour
 {
@@ -26,13 +27,12 @@ public class Jugador : MonoBehaviour
     public void ModificarVida(int puntos)
     {
         perfilJugador.Vida += puntos;
-        
+
         if (perfilJugador.Vida < 0) { perfilJugador.Vida = 0; }
         else if (perfilJugador.Vida > 10) { perfilJugador.Vida = 10; }
-        
+
         OnLivesChanged.Invoke(perfilJugador.Vida);
         //Debug.Log("la vida del jugador es: " + perfilJugador.Vida);
-
     }
 
 
@@ -43,6 +43,14 @@ public class Jugador : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // si el jugador choca con la lava, reinicia el nivel
+        if (collision.gameObject.CompareTag("Lava"))
+        {
+            Debug.Log("Toco la lava");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene()."Nivel1");
+            SceneManager.LoadScene("Nivel1");
+
+        }
         if (!collision.gameObject.CompareTag("Meta")) { return; }
 
         Debug.Log("GANASTE");
